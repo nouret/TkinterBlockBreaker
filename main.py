73d75311d05dc892fc9a3ab0bb58511de5e2e1b7
nouret <rfnourettuz@gmail.com>
2016-03-29 19:09:50 +0300

    end of lesson 1

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ XCircle = 300
 YCircle = 300
 XRectangle = 300
 r = 25
+d = 30
 DXCircle = 2
 DYCircle = 3
 
@@ -18,13 +19,21 @@ def function(x, y):
 	for i in range(x):
 		print(y)
 
-def VertHit(x = float("nan")):
+def UpHit(x = float("nan")):
 	global DYCircle
-	DYCircle *= -1
+	DYCircle = -abs(DYCircle)
 
-def HorHit(x = float("nan")):
+def DownHit(x = float("nan")):
+	global DYCircle
+	DYCircle = abs(DYCircle)
+
+def LeftHit(x = float("nan")):
 	global DXCircle
-	DXCircle *= -1
+	DXCircle = abs(DXCircle)
+
+def RightHit(x = float("nan")):
+	global DXCircle
+	DXCircle = -abs(DXCircle)
 
 def rectanglemove(event):
 	global XRectangle
@@ -33,18 +42,22 @@ def rectanglemove(event):
 		XRectangle = 3 * r
 	if XRectangle > size - 3 * r:
 		XRectangle = size - 3 * r
-	canvas.coords(rectangle, XRectangle - 3 * r, size, XRectangle + 3 * r, size - 10)
+	canvas.coords(rectangle, XRectangle - 3 * r, size, XRectangle + 3 * r, size - d)
 	canvas.update()
 
 def move():
-	global XCircle, YCircle
+	global XCircle, YCircle, XRectangle
 	XCircle += DXCircle
 	YCircle += DYCircle
 	canvas.coords(circle, XCircle - r, YCircle - r, XCircle + r, YCircle + r)
-	if XCircle <= r or size - r <= XCircle:
-		HorHit()
-	if YCircle <= r or size - r <= YCircle:
-		VertHit()
+	if YCircle <= r:
+		DownHit()
+	if size - r <= YCircle or (XRectangle - 3 * r <= XCircle <= XRectangle + 3 * r and size - r - d <= YCircle):
+		UpHit()
+	if XCircle <= r:
+		LeftHit()
+	if size - r <= XCircle:
+		RightHit()
 	canvas.update()
 	canvas.after(50, move)
 
@@ -53,7 +66,7 @@ canvas = tkinter.Canvas(root, width=size, height=size)
 canvas.bind("<Motion>", rectanglemove)
 canvas.pack()
 circle = canvas.create_oval(XCircle-r, YCircle-r, XCircle+r, YCircle+r, fill='red')
-rectangle = canvas.create_rectangle(XRectangle - 3 * r, size, XRectangle + 3 * r, size - 10, fill = "green")
+rectangle = canvas.create_rectangle(XRectangle - 3 * r, size, XRectangle + 3 * r, size - d, fill = "green")
 helpcircle = canvas.create_oval(10, 20, 20, 30, fill='green')
 root.after(50, move)
 root.mainloop()
